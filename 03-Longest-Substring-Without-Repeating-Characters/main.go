2c5a91be4c84b06cc72b85d54944ff412f12c159
harry <harry@nil.com>
2024-04-02 21:49:01 +0800

    3. Longest Substring Without Repeating Characters

---
+++ 03-Longest-Substring-Without-Repeating-Characters/main.go
@@ -0,0 +1,20 @@
+package _3_Longest_Substring_Without_Repeating_Characters
+
+func lengthOfLongestSubstring(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+	pre, res := 0, 1
+	m := make(map[byte]int, len(s))
+	for cur := 0; cur < len(s); cur++ {
+		if _, ok := m[s[cur]]; ok {
+			pre = max(pre, m[s[cur]]+1)
+			m[s[cur]] = cur
+			res = max(res, cur-pre+1)
+		} else {
+			m[s[cur]] = cur
+			res = max(res, cur-pre+1)
+		}
+	}
+	return res
+}
