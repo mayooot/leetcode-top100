8f060eebe8c4b3edd6b1d85fe5f3bb55d411dc0e
Harry Li <bertram_li@163.com>
2024-04-02 14:46:14 +0800

    update 02-AddTwoNumbers

--- 02-AddTwoNumbers/main.go
+++ 02-AddTwoNumbers/main.go
@@ -6,39 +6,51 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	pre := &ListNode{}
-	cur := pre
+	l3 := &ListNode{Val: 123}
+	cur := l3
 	increment := 0
 	for l1 != nil && l2 != nil {
 		sum := l1.Val + l2.Val + increment
 		increment = sum / 10
 		sum = sum % 10
+
+		l1, l2 = l1.Next, l2.Next
+
 		cur.Next = &ListNode{Val: sum}
 		cur = cur.Next
-
-		l1 = l1.Next
-		l2 = l2.Next
 	}
+
+	// 两个链表节点相同，但是有进位
+	// if l1 == nil && l2 == nil && increment == 1 {
+	// 	cur.Next = &ListNode{Val: 1}
+	// }
 
 	for l1 != nil {
 		sum := l1.Val + increment
 		increment = sum / 10
 		sum = sum % 10
+
+		l1 = l1.Next
+
 		cur.Next = &ListNode{Val: sum}
 		cur = cur.Next
-		l1 = l1.Next
 	}
+
 	for l2 != nil {
 		sum := l2.Val + increment
 		increment = sum / 10
 		sum = sum % 10
+
+		l2 = l2.Next
+
 		cur.Next = &ListNode{Val: sum}
 		cur = cur.Next
-		l2 = l2.Next
 	}
 
+	// 两个链表节点相同，但是有进位
 	if l1 == nil && l2 == nil && increment == 1 {
 		cur.Next = &ListNode{Val: 1}
 	}
-	return pre.Next
+
+	return l3.Next
 }
